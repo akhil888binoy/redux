18433260eacfadada5d1c7a3a32716afa3006b24
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-07-05 20:50:43 +0530

    initial

---
+++ src/features/posts/PostAuthor.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
+import { selectAllUsers } from "../users/usersSlice";
+const PostAuthor = (userId) => {
+  const users = useSelector(selectAllUsers);
+  const author = users.find((user) => user.id === userId);
+
+  return <span>by {author ? author.name : "unknown author"}</span>;
+};
+
+export default PostAuthor;
